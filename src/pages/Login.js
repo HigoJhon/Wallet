b953e92946ce2e30df24815a3986b7e059992257
HigoJhon <higojhon98@gmail.com>
2022-12-08 19:40:58 -0300

    criando a page Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const six = 6;
+      const { email, password } = this.state;
+      const validation = (
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+      );
+      const valiEmail = email.match(validation);
+      const valiPass = password.length < six;
+      const valid = !valiEmail || valiPass;
+      console.log(valid);
+      this.setState({ isDisabled: valid });
+    });
+  };
+
+  handleSubmit = () => {
+    const { history } = this.props;
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="email-input"
+          type="email"
+          // value={ email }
+          name="email"
+          placeholder="Digite o seu email"
+          onChange={ this.handleChange }
+        />
+        <br />
+        <input
+          data-testid="password-input"
+          placeholder="senha"
+          type="password"
+          name="password"
+          // value={ password }
+          onChange={ this.handleChange }
+        />
+        <br />
+        <button
+          type="button"
+          disabled={ isDisabled }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
