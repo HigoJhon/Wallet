d3150a7450f1ea2a16267e0c29395d179fbf753c
HigoJhon <higojhon98@gmail.com>
2022-12-15 16:57:32 -0300

    Criando as descriçoẽs de Table

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Wallet extends React.Component {
       <>
         <div data-testid="email-field">{`Email: ${email}`}</div>
         <Header />
+        <Table />
       </>
     );
   }
