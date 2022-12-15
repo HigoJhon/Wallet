87b228257207161695b90066d4067e7257974a6d
HigoJhon <higojhon98@gmail.com>
2022-12-15 20:59:23 -0300

    Criando a tapela de aplicaço

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </table>
+      <>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
+        <table>
+          {
+            expenses.map((a) => (
+              <tbody key={ a }>
+                <tr>
+                  <td>{a.description}</td>
+                  <td>{a.tag}</td>
+                  <td>{a.method}</td>
+                  <td>{Number(a.value).toFixed(2)}</td>
+                  <td>{a.exchangeRates[a.currency].name}</td>
+                  <td>{Number(a.exchangeRates[a.currency].ask).toFixed(2)}</td>
+                  <td>{Number(a.value * a.exchangeRates[a.currency].ask).toFixed(2)}</td>
+                  <td>Real</td>
+                </tr>
+              </tbody>
+
+            ))
+          }
+        </table>
+      </>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
